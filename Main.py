08cce4580d941ba6e368f3cd7f5897a71a16b7ff
Hazyninja <white.edward1@gmail.com>
2024-08-28 20:27:08 -0400

    Added dynamic background and label styling

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog, ttk
+from PIL import Image, ImageTk
 import threading
 import os
 import shutil
@@ -10,7 +11,8 @@ import shutil
  button to begin the sort. The files are sorted into three folders 
  that are created if needed inside the parent folder."""
 
-# FIXME: Adjust grid on buttons
+# Added dynamic background
+# Added styling to labels
 
 
 class GUI(Tk):
@@ -25,10 +27,28 @@ class GUI(Tk):
         self.start_Button = None
         self.browse_Button = None
         self.path = None
-        self.geometry('550x400')
+        self.geometry('400x360')
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.title('Rabbithole Sorter')
+
+        # Canvas widget for background
+        self.canvas = Canvas(self, width=400, height=360)
+        self.canvas.grid(row=0, column=0, rowspan=11, columnspan=2, sticky='nsew')
+
+        # Load image for background
+        self.original_image = Image.open('Images/background.png')
+        self.bg_image = ImageTk.PhotoImage(self.original_image.resize((400, 360)))
+
+        # Attach image to canvas
+        self.bg_image_obj = self.canvas.create_image(
+            0, 0,
+            anchor='nw',
+            image=self.bg_image,
+        )
+
+        self.bind('<Configure>', self.resize_image)
+
         self.create_browse_button()
         self.define_dir()
         # Initialize progress bar
@@ -43,10 +63,17 @@ class GUI(Tk):
             'TButton',
             background='#1DAF28',
             foreground='Red',
-            font=('Helvetica, 12'),
+            font=('High Tower Text', 12),
             relief='groove',
             highlightthickness=10,
             focuscolor='Black'
+        )
+
+        style.configure(
+            'TLabel',
+            foreground='RoyalBlue4',
+            font=('High Tower Text', 12),
+
         )
 
     def create_browse_button(self):
@@ -82,13 +109,13 @@ class GUI(Tk):
         ]
         # Retrieve the index and value for proper grid placement
         for i, file_type in enumerate(file_types):
-            label = Label(
+            label = ttk.Label(
                 self,
                 text=f'{file_type} Directory Name: '
             )
-            label.grid(row=i+1, column=0, padx=50, pady=5, sticky='e')
+            label.grid(row=i+1, column=0, padx=10, pady=1, sticky='e')
             entry = Entry(self)
-            entry.grid(row=i+1, column=1, padx=50, pady=5, sticky='w')
+            entry.grid(row=i+1, column=1, padx=10, pady=1, sticky='w')
             self.user_dirs[file_type] = entry
 
     def create_start_button(self, path_value):
@@ -101,6 +128,20 @@ class GUI(Tk):
             width=10,
         )
         self.start_Button.grid(row=5, column=5, pady=5, sticky='e')
+
+    def resize_image(self, event):
+        # Resizes the background dynamically
+        self.canvas.config(width=event.width, height=event.height)
+
+        # Resize background based on window size
+        resized_image = self.original_image.resize((event.width, event.height))
+        self.bg_image = ImageTk.PhotoImage(resized_image)
+
+        # update canvas with adjusted image
+        self.canvas.itemconfig(self.bg_image_obj, image=self.bg_image)
+
+
+
 
     def start_sort(self, path_value):
         # Main function to sort files
@@ -173,10 +214,10 @@ class GUI(Tk):
 
         if count == num_files:
             self.finish_success = Label(text='Success!')
-            self.finish_success.grid(row=5, column=1, pady=5)
+            self.finish_success.grid(row=9, column=1, pady=5)
         else:
             self.finish_error = Label(text='Task Failed')
-            self.finish_error.grid(row=5, column=1, pady=5)
+            self.finish_error.grid(row=9, column=1, pady=5)
 
 
 if __name__ == '__main__':
